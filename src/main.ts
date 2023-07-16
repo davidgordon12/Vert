cf8f6fe3a2434e4858c8e48654ed3e0abf9fc787
tehtko <moderndsnsam@gmail.com>
2023-07-16 19:29:41 -0400

    Create main editor components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-window.addEventListener("DOMContentLoaded", () => {
+let editor: HTMLTextAreaElement = (document.getElementById('editor-textarea') as HTMLTextAreaElement);
+let explorer: HTMLTextAreaElement = (document.getElementById('explorer') as HTMLTextAreaElement); 
+let toolbar: HTMLTextAreaElement = (document.getElementById('toolbar') as HTMLTextAreaElement);
 
+window.addEventListener("DOMContentLoaded", () => {
+    setEditorSize();
 });
+
+function resizeExplorer(): void {
+    // resize explorer then reset the editor's size: WIP
+
+    setEditorSize(); 
+}
+
+
+function setEditorSize(): void {
+    let explorerWidth: Number = explorer.offsetWidth;
+    let toolbarWidth: Number = toolbar.offsetWidth;
+
+    let offset: Number = +explorerWidth + +toolbarWidth;
+
+    editor.style.width = (document.body.clientWidth - +offset).toString() + "px";
+}
