b187d6f435c7a022e3d144a844b01f75992c4db2
davidgordon12 <davidgordon12@live.ca>
2023-07-24 14:41:37 -0400

    move readEntry method

--- src/ipc.ts
+++ src/ipc.ts
@@ -1,6 +1,10 @@
 import { DirectoryContent } from "./types";
 import { invoke } from "@tauri-apps/api/tauri"
 
-export default async function openDirectory(path: string): Promise<Array<DirectoryContent>> {
+export async function openDirectory(path: string): Promise<Array<DirectoryContent>> {
     return await invoke('open_directory', { path: path })
+}
+
+export async function readEntry(path: String): Promise<string> {
+    return await invoke('read_entry', { path: path } )
 }
