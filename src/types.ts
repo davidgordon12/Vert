a74f7457381ee4ce3f733bb02955c807e0bcff3b
tehtko <moderndsnsam@gmail.com>
2023-07-26 18:42:21 -0400

    Annotate DirectoryContent interface

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
-export type DirectoryEntityType = "file" | "directory";
-export type DirectoryContentType = "File" | "Directory";
-
+/* 
+ * Object.entries(<DirectoryContent>)[0][0][0] will unwrap Entity type, return F if file, D if dir
+ * Object.entries(<DirectoryContent>)[0][1][0] will return the entity name with file extension (i.e. 'tsconfig.json')
+ * Object.entries(<DirectoryContent>)[0][1][1] will return full path of the entity including file extension ('C:\Matcha\Vert\node_modules)
+ */
 export interface DirectoryContent {
     [key: string]: [string, string]; // Key will be either "Directory" or "File"
 }
