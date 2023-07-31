da694ec5d612802f1876acd6ee5c72fc584c9011
tehtko <moderndsnsam@gmail.com>
2023-07-31 17:57:11 -0400

    Remove dupliucate epxlorer.ts (result from bad merge)

--- src/components/explorer.ts
+++ src/components/explorer.ts
@@ -1,46 +1,36 @@
-import interact from "interactjs";
-import { homeDir } from "@tauri-apps/api/path";
-import { open } from "@tauri-apps/api/dialog";
-import { openDirectory, readEntry } from "../ipc";
-import { DirectoryContent } from "../types";
-import { setEditorSize } from "./editor";
+import { homeDir } from '@tauri-apps/api/path';
+import { open } from '@tauri-apps/api/dialog';
+import { openWorkspace, readFile } from "../ipc";
 
-let selectedDir = "";
-let openedFile = ""
+type Workspace = {
+  dirs: Array<DirEntry> | undefined
+  files: Array<FileEntry> | undefined
+};
 
-interact(explorer).resizable({
-  edges: { top: false, left: true, bottom: false, right: false },
-  listeners: {
-    move: function (event: any) {
-      let { x, y } = event.target.dataset;
+type DirEntry = {
+  key: string,
+  path: string,
+  children: Array<FileEntry>,
+};
 
-      x = (parseFloat(x) || 0) + event.deltaRect.left;
-      y = (parseFloat(y) || 0) + event.deltaRect.top;
-
-      explorer.style.maxWidth = "80%";
-
-      Object.assign(event.target.style, {
-        width: `${event.rect.width}px`,
-      });
-
-      Object.assign(event.target.dataset, { x, y });
-
-      setEditorSize();
-    },
-  },
-});
+type FileEntry = {
+  key: string,
+  path: string,
+  content: string,
+};
 
 openFolderBtn.onclick = async () => {
-  const selected = (await open({
+  console.log("dasdas ")
+  const selected = await open({
     directory: true,
     multiple: false,
     defaultPath: await homeDir(),
-  })) as string;
+  }) as string;
 
-  let files: Array<DirectoryContent> = await openDirectory(selected);
-
-  console.log(files)
-};
-
-function processEntries(files: Array<DirectoryContent>) {
+  let oldWorkspace = document.getElementsByTagName('li');
+  for(let i = oldWorkspace.length-1; i >= 0; --i) {
+    oldWorkspace[i].remove()
+  }
+ 
+  console.log(await openWorkspace(selected));
 }
