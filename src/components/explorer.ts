004cfb08c975fe999c82b03cba3c7e565ea83c8a
tehtko <moderndsnsam@gmail.com>
2023-08-01 16:54:58 -0400

    Update exports

--- src/components/explorer.ts
+++ src/components/explorer.ts
@@ -1,36 +1,28 @@
 import { homeDir } from '@tauri-apps/api/path';
 import { open } from '@tauri-apps/api/dialog';
-import { openWorkspace, readFile } from "../ipc";
+import { openWorkspace, openFile } from "../ipc";
 
-type Workspace = {
+export type Workspace = {
   dirs: Array<DirEntry> | undefined
   files: Array<FileEntry> | undefined
 };
 
-type DirEntry = {
+export type DirEntry = {
   key: string,
   path: string,
-  children: Array<FileEntry>,
+  children: Array<FileEntry> | undefined,
 };
 
-type FileEntry = {
+export type FileEntry = {
   key: string,
   path: string,
   content: string,
 };
 
 openFolderBtn.onclick = async () => {
-  console.log("dasdas ")
   const selected = await open({
     directory: true,
     multiple: false,
     defaultPath: await homeDir(),
   }) as string;
-
-  let oldWorkspace = document.getElementsByTagName('li');
-  for(let i = oldWorkspace.length-1; i >= 0; --i) {
-    oldWorkspace[i].remove()
-  }
- 
-  console.log(await openWorkspace(selected));
 }
