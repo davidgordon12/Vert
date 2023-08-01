99f6e98d24a9037003cd76b765d440905cc7a8b8
tehtko <moderndsnsam@gmail.com>
2023-08-01 16:54:32 -0400

    Update methodnames and imports

--- src/ipc.ts
+++ src/ipc.ts
@@ -1,9 +1,10 @@
 import { invoke } from "@tauri-apps/api/tauri"
+import { Workspace, FileEntry, DirEntry } from "./components/explorer"
 
-export async function openWorkspace(path: string): Promise<Array<String>> {
+export async function openWorkspace(path: string): Promise<Workspace> {
     return await invoke('open_workspace', { path: path })
 }
 
-export async function readFile(path: String): Promise<string> {
-    return await invoke('read_file', { path: path } )
+export async function openFile(path: String): Promise<FileEntry> {
+    return await invoke('open_file', { path: path } )
 }
